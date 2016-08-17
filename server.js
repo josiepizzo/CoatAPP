ac8480378982f1855c17fae0805e94703706a765
Joshua Cassell <Joshua Cassell>
2016-08-16 21:03:22 -0400

    restructured folder

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 
-var models = require('./models')
-//var inventory = require('./models')['inventory'];
-//var transaction = require('./models')['transaction'];
-
-console.log('i am about to create the tables');
-models.sequelize.sync({force:true});
-console.log('i just created the tables');
-//user.sync({force:true});
-//inventory.sync({force:true});
-//transaction.sync({force:true});
-
-
 var app = express();
+//var app = module.exports = express.createServer();
+//created express server
+
+
 
 //line below allows anything that is in this folder to be accessed via the internet
 //inside the public folder the css file and any images can be stored
@@ -26,11 +18,34 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-app.engine('handlebars',handlebars({
-	defaultLayout: 'main'
-}));
 
-app.set('view engine','handlebars');
+
+app.get('/', function(req, res){
+  res.render('index.handlebars');
+});
+
+app.get('/donator', function(req, res){
+  res.render('donator.handlebars');
+});
+
+app.get('/customer', function(req, res){
+  res.render('customer.handlebars');
+});
+app.get('/', function(req, res){
+  res.render('donator.handlebars');
+});
+
+app.get('/coats', function(req, res){
+  res.render('coats.handlebars');
+});
+
+
+
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+//will be the skeleton of every webpage
+
+app.set('view engine', 'handlebars');
 
 var port = process.env.Port || 3000;
 app.listen(port, function(){
@@ -38,7 +53,7 @@ app.listen(port, function(){
 });
 
 //=========================================================
-//routes to be put in a seperate controller folder 
+//routes to be put in a seperate  controller folder 
 //one file for HTML or new page routes
 //another for same page processing or API routes
 //=========================================================
